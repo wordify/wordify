8b294161c1171d9e92fbb8b082b013870cce6478
Andreas Rytman <andreas@rytman.dk>
2013-07-17 19:55:43 +0200

    styling

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -1,7 +1,8 @@
 <!doctype html>
 <html>
 	<head>
-		<title></title>
+		<title>Wordify - Make your words become real!</title>
+		<link href="/css/home.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
 		<div class="container">
