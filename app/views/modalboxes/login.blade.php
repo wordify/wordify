2f1729d6fb67ac3b27e1b185e271f220121107e6
Jonas Hansen <kyllesdk@gmail.com>
2013-07-21 20:39:59 +0200

    Corrected the post url to '/login'

--- app/views/modalboxes/login.blade.php
+++ app/views/modalboxes/login.blade.php
@@ -1,6 +1,6 @@
 <div class="modal" id="loginModal">
 	<h1 id='modalTitle'>Login</h1>
-	{{ Form::open(array('url' => '/')); }}
+	{{ Form::open(array('url' => '/login')); }}
 		{{ Form::text('username', 'USERNAME', array('class' => 'loginUsername')) }}
 		{{ Form::password('password', array('class' => 'loginPassword')) }}
 		{{ Form::submit('Login', array('class' => 'modalSubmitBtn')) }}
