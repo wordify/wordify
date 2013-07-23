ebd354f0db2171003c309c5b1bc82cf5123f5ab8
Jonas Hansen <kyllesdk@gmail.com>
2013-07-23 21:54:48 +0200

    Updated seed file on user kyllesdk

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -12,14 +12,15 @@ class UsersTableSeeder extends Seeder {
         			'username' => 'kyllesdk',
         			'name'	=>	'Jonas Hansen',
         			'email'	=>	'kyllesdk@gmail.com',
-        			'password'	=>	'1234',
+        			'password'	=>	Hash::make('7arm1dnp48'),
         			'country'	=>	'Denmark',
         			'job'	=>	'CEO Wordify',
         			'website'	=>	'http://wordify.me',
         			'userStatus'	=>	'1',
         			'created_at'	=>	'2013-02-18 21:20:02',
         			'updated_at'	=>	'2013-02-18 21:20:02',
-        			'profilePicture'	=>	'PictureHere'
+        			'profilePicture'	=>	'https://graph.facebook.com/1320819338/picture?width=400&height=400',
+                    'provider_id' => '1320819338'
         		)
         );
 
