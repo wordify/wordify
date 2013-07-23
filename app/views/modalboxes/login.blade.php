e484158bb3cfcc3d1678409bbed0b7eb905c1bdc
Jonas Hansen <kyllesdk@gmail.com>
2013-07-23 21:14:19 +0200

    Added styling to the modal box

--- app/views/modalboxes/login.blade.php
+++ app/views/modalboxes/login.blade.php
@@ -1,10 +1,12 @@
 <div class="modal" id="loginModal">
 	<h1 id='modalTitle'>Login</h1>
 	{{ Form::open(array('url' => '/login')); }}
-		{{ Form::text('username', 'USERNAME', array('class' => 'loginUsername')) }}
-		{{ Form::password('password', array('class' => 'loginPassword')) }}
+		<input type="text" class="modalInputField" id="username" placeholder="USERNAME" required>
+		<input type="password" class="modalInputField" id="password" placeholder="PASSWORD" required>
+	<div class="modalButtonContainer">
 		{{ Form::submit('Login', array('class' => 'modalSubmitBtn')) }}
+		<span class='modalORText'>OR</span>
+		<a href="/login/facebook" class='facebookLoginBtn'>LOGIN WITH FACEBOOK</a>
+	</div>
 	{{ Form::close(); }}
-	OR
-	<a href="/login/facebook">LOGIN WITH FACEBOOK</a>
 </div>
