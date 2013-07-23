9241c08f1887299f0e4017433566e1efc9c0e9d8
Jonas Hansen <kyllesdk@gmail.com>
2013-07-23 21:44:40 +0200

    added styling to all the input fields

--- app/views/modalboxes/login.blade.php
+++ app/views/modalboxes/login.blade.php
@@ -1,8 +1,8 @@
 <div class="modal" id="loginModal">
 	<h1 id='modalTitle'>Login</h1>
 	{{ Form::open(array('url' => '/login')); }}
-		<input type="text" class="modalInputField" id="username" placeholder="USERNAME" required>
-		<input type="password" class="modalInputField" id="password" placeholder="PASSWORD" required>
+		<input type="text" class="modalLoginInputField" id="username" name="username" placeholder="USERNAME" required>
+		<input type="password" class="modalLoginInputField" id="password" name="password" placeholder="PASSWORD" required>
 	<div class="modalButtonContainer">
 		{{ Form::submit('Login', array('class' => 'modalSubmitBtn')) }}
 		<span class='modalORText'>OR</span>
