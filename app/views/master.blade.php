4d62c210cb5934f7efdf47a193c434adb7cf4a50
Jonas Hansen <kyllesdk@gmail.com>
2013-07-23 22:11:31 +0200

    Added logo

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -24,6 +24,7 @@
 				@if(Auth::check())
 					<li><a href="{{ URL::to('logout') }}">Log out</a></li>
 				@else
+					<img src="/images/logo/Wordify-logo_50px.png" id="logo"/>
 					<li id="loginButton"><a href='#'>Login</a></li>
 					<li id="createUserButton"><a href='#'>Register</a></li>
 				@endif
