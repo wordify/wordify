be99d7898e8da5bcc7c3ec3bff864272c340a3a0
frejlev <andreas@rytman.dk>
2013-07-29 22:30:56 +0200

    number of guesses now autoupdate

--- app/views/comments/index.blade.php
+++ app/views/comments/index.blade.php
@@ -1,6 +1,6 @@
 <script>
 	@foreach($comments as $comment)
 		$('#commentBlock{{ $comment->word_id }}').find('.commentList').append('<img class="point" src="http://wordify.me/application/images/icons/point_grey.png"><div class="acommentBox"><span class="username {{ $comment->user_id }}"><b style="font-size: 25px;">afr</b></span><br> {{ $comment->comment }}</div>');
-		$('guessCount {{ $comment->word_id }}').text(''+$('#commentBlock{{ $comment->word_id }}').find('.commentList .acommentBox').length+'');
+		$('.guessCount.{{ $comment->word_id }}').text(''+$('#commentBlock{{ $comment->word_id }}').find('.commentList .acommentBox').length+'');
 	@endforeach
 </script>
